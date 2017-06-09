e85af4e51e218579277361f24b60cb742cc6c787
illestboy <illest.gt@gmail.com>
2017-06-10 01:00:12 +0500

    Code Optimized

--- src/com/illestboy/Encoder.java
+++ src/com/illestboy/Encoder.java
@@ -2,9 +2,6 @@ package com.illestboy;
 
 import java.util.*;
 
-/**
- * Created by illestboy on 05.06.17.
- */
 public class Encoder {
 
     private static class Node implements Comparable<Node> {
@@ -17,128 +14,115 @@ public class Encoder {
 
         private Node rightNode;
 
-        public Node(Character character, int frequency) {
+        Node(Character character, int frequency) {
             this.frequency = frequency;
             this.character = character;
         }
 
-        public Node(Node leftNode, Node rightNode) {
+        Node(Node leftNode, Node rightNode) {
             this.character = null;
             this.leftNode = leftNode;
             this.rightNode = rightNode;
-            this.frequency = leftNode.getFrequency() + rightNode.getFrequency();
+            this.frequency = leftNode.frequency + rightNode.frequency;
         }
 
-        public int getFrequency() {
-            return frequency;
-        }
-
-        public void setFrequency(int frequency) {
-            this.frequency = frequency;
-        }
-
-        public Character getCharacter() {
+        Character getCharacter() {
             return character;
         }
 
-        public void setCharacter(Character character) {
-            this.character = character;
-        }
-
-        public Node getLeftNode() {
+        Node getLeftNode() {
             return leftNode;
         }
 
-        public void setLeftNode(Node leftNode) {
-            this.leftNode = leftNode;
-        }
-
-        public Node getRightNode() {
+        Node getRightNode() {
             return rightNode;
-        }
-
-        public void setRightNode(Node rightNode) {
-            this.rightNode = rightNode;
         }
 
         @Override
         public int compareTo(Node o) {
-            int result = this.frequency.compareTo(o.getFrequency());
-            //if (result == 0 && this.character != null && o.getCharacter() != null) {
-            //    result = o.getCharacter().compareTo(this.character);
-            //}
-            return result;
+            return this.frequency.compareTo(o.frequency);
         }
 
         @Override
         public String toString() {
-            return String.valueOf(this.getFrequency());
+            return String.valueOf(this.frequency);
         }
     }
 
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+        StringBuilder text = new StringBuilder();
+        while (!input.equals("") && scanner.hasNextLine()) {
+            text.append(input);
+            input = scanner.nextLine();
+        }
+        scanner.close();
+
+        Encoder encoder = new Encoder();
+        encoder.setText(text.toString());
+        encoder.encode();
+        System.out.println(encoder.getTable());
+        System.out.println(encoder.getEncodedText());
+    }
+
+    private String inputText;
+
     private Map<Character, Integer> chars;
 
-    private Map<Character, String> table = new HashMap<>();
+    private Map<Character, String> table;
 
-    private PriorityQueue<Node> queue = new PriorityQueue<>();
+    private PriorityQueue<Node> queue;
 
-    private Node root;
-
-    private static Set<Character> characters;
+    private static Set<Character> validCharacters;
 
     public Encoder() {
         chars = new HashMap<>();
-        characters = new HashSet<>();
+        validCharacters = new HashSet<>();
+        queue = new PriorityQueue<>();
+        table = new LinkedHashMap<>();
+        inputText = "";
         initializeAllowedCharacters();
     }
 
     private void initializeAllowedCharacters() {
-        for (char c = 'a'; c <= 'z'; c++) {
-            characters.add(c);
-        }
-
-        for (char c = 'A'; c <= 'Z'; c++) {
-            characters.add(c);
-        }
-
-        for (char c = 'А'; c <= 'я'; c++) {
-            characters.add(c);
-        }
-        for (char c = '0'; c <= '9'; c++) {
-            characters.add(c);
-        }
-
-        characters.add('!'); // Убрать
-        characters.add(',');
-        characters.add('.');
-        characters.add(' ');
-        characters.add(':');
-        characters.add('Ё');
-        characters.add('ё');
+        for (char c = 'a'; c <= 'z'; c++)
+            validCharacters.add(c);
+        for (char c = 'A'; c <= 'Z'; c++)
+            validCharacters.add(c);
+        for (char c = 'А'; c <= 'я'; c++)
+            validCharacters.add(c);
+        for (char c = '0'; c <= '9'; c++)
+            validCharacters.add(c);
+        validCharacters.add(',');
+        validCharacters.add('.');
+        validCharacters.add(' ');
+        validCharacters.add(':');
+        validCharacters.add('Ё');
+        validCharacters.add('ё');
     }
 
-    public void encodeChars() {
-        buildTree();
+    public void encode() {
+        this.chars.forEach((key, value) -> this.queue.add(new Node(key, value)));
+        while (queue.size() > 1) {
+            this.queue.add(new Node(queue.poll(), queue.poll()));
+        }
+        buildTable(queue.poll(), "");
     }
 
-    private void buildTree() {
-        chars = sortMap(chars);
-        fillQueue();
-        if (queue.size() > 1 ) {
-            while (queue.size() > 2) {
-                this.queue.add(new Node(queue.poll(), queue.poll()));
+    public String getEncodedText() {
+        StringBuilder result = new StringBuilder();
+        for (char c : inputText.toCharArray()) {
+            if (validCharacters.contains(c)) {
+                result.append(this.table.get(c));
             }
-            this.root = new Node(queue.poll(), queue.poll());
-        } else if (queue.size() == 1) {
-            this.root = queue.poll();
         }
-        buildTable(root, "");
-        System.out.println(table);
+        return result.toString();
     }
 
     private void buildTable(Node node, String code){
         if (node.getCharacter() != null) {
-            table.put(node.getCharacter(), code);
+            table.put(node.getCharacter(), code.length() > 0 ? code : "0");
             return;
         }
         if (node.getLeftNode() != null) {
@@ -149,33 +133,16 @@ public class Encoder {
         }
     }
 
-    private boolean canVisit(Node node, List<Node> visited) {
-        return (!visited.contains(node) && node.getLeftNode() != null && node.getRightNode() != null);
-    }
-
-    private void fillQueue() {
-        this.chars.forEach((key, value) -> this.queue.add(new Node(key, value)));
-    }
-
-    public void addChar(Character c) {
-        if (characters.contains(c)) {
-            chars.put(c, chars.getOrDefault(c, 0) + 1);
+    public void setText(String s) {
+        this.inputText = s;
+        for (char c : s.toCharArray()) {
+            if (validCharacters.contains(c)) {
+                chars.put(c, chars.getOrDefault(c, 0) + 1);
+            }
         }
     }
 
-    public Map<Character, Integer> getChars() {
-        return Collections.unmodifiableMap(chars);
-    }
-
-    public static <K, V extends Comparable<? super V>> Map<K, V> sortMap( Map<K, V> map ) {
-        List<Map.Entry<K, V>> list =
-                new LinkedList<>(map.entrySet());
-        Collections.sort( list, Comparator.comparing(o -> (o.getValue())));
-
-        Map<K, V> result = new LinkedHashMap<>();
-        for (Map.Entry<K, V> entry : list) {
-            result.put( entry.getKey(), entry.getValue() );
-        }
-        return result;
+    public Map<Character, String> getTable() {
+        return Collections.unmodifiableMap(table);
     }
 }
